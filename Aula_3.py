1df64eda126385fb5b386cf52ff3e281a973932a
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-19 13:56:38 -0300

    Primeiro commit

--- Aula_3.py
+++ Aula_3.py
@@ -8,4 +8,13 @@ print(type(10 == 11))
 
 #float
 
-print(1+2.5)
+print(1+2.5) # inteiro + float = float
+print(1.5) # float
+
+# int
+#int = numero inteiros podendo ser positivo(+) ou negativo(-)
+
+#strings ou Str
+
+print("oi") # Str
+print ("12") # continua sendo str, pois está dentro de "", então sempre que estiver assim, o resultado é sempre uma String.
