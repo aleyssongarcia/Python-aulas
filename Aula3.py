71d2ebabb05c203eb28d61b2655a3d1cc3a4774c
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-19 13:55:02 -0300

    Primeiro commit

---
+++ Aula3.py
@@ -0,0 +1,11 @@
+# Bolean
+# class "type" faz mostrar o tipo do objeto, abaixo vemos bolean
+
+print(10 == 10) #Sim -> True (Verdadeiro)
+print( 10 == 11) #Não -> False (Falso)
+print(type(10 == 10))
+print(type(10 == 11))
+
+#float
+
+print(1+2.5)
---
+++ Aula_2.py
@@ -0,0 +1,5 @@
+print(11) # int = numero inteiro
+print(-11) # int = numero inteiro
+print(1.1) # float = numero com ponto flutuante
+print("a") # Str = texto com caractere 
+print(True) # boll (bolean) = true ou false
