6f658740be844515a40a7d89cf2a3d17fefadeee
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-19 13:45:13 -0300

    Primeiro commit

---
+++ Aula_1.py
@@ -0,0 +1,12 @@
+print('mensagem') # imprime uma mensagem no painel.
+
+# Aspas simples
+print('seu nome') # imprime - seu nome
+print('seu "nome"') # imprime - seu "nome"
+
+#Escape
+print("Seu \"nome") # usando o \, vc mostrara o " no painel - seu " nome
+
+#r
+print(r"Seu \"nome\"") #utilizado r, para mostrar cacteres especiais. imprime - sei \"nome\"
+
