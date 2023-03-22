a0d5e881eecabb67b073ad86e50b67dff3499e09
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-22 16:06:08 -0300

    Primeiro commit

--- Aula_4.py
+++ Aula_4.py
@@ -7,3 +7,12 @@ um valor a variavel.
 
 nome = 'seunome'  # criei uma variavel e atriubir ela ao valor de string " seunome"
 idade = 20 # criei uma variavel e atribuir a ela o valor de um int 20.
+verdade = 20 == 20 # Criei uma variavel e atribuir valores de int, mas que me dara um resultado de boolean.
+numero = 5/2 # Criei uma variavel e atruir valores de int, mas que me dara um resultado em float
+
+print(type(verdade)) # = <class 'bool'>
+print(type(numero)) # = <class 'float'>
+print(type(idade)) # = <class 'int'>
+print(type(nome)) # = <class 'str'>
+
+ # type me diz a class.
