c9c9b3422fd699444859d37c8c78f8f51f5d55ef
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-22 16:01:16 -0300

    Primeiro commit

---
+++ Aula_4.py
@@ -0,0 +1,9 @@
+"""
+Variaveis são utitlizadas para salvar algo na memoria do computador.
+Regras: iniciar variavel com letra maiuscula ( tudo maiusculo é considerado Constante)
+O sinal d e = é o operador de atribuiçãi, é utilizado para atribuir
+um valor a variavel.
+"""
+
+nome = 'seunome'  # criei uma variavel e atriubir ela ao valor de string " seunome"
+idade = 20 # criei uma variavel e atribuir a ela o valor de um int 20.
